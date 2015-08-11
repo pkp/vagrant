cad63f8282a2291be0a9adbf47cc32027e3630b7
Michael Felczak <michael.pkp@gmail.com>
2015-08-11 16:32:52 -0700

    Initial steps for secure setup

--- scripts/ojs.sh
+++ scripts/ojs.sh
@@ -1,19 +1,26 @@
 echo "Installing OJS"
 
+# Create ojs user and home dir
+adduser ojs
+
 # Set up the OJS database
 echo "CREATE DATABASE ojs" | mysql -uroot -pojs
 echo "CREATE USER 'ojs'@'localhost' IDENTIFIED BY 'ojs'" | mysql -uroot -pojs
 echo "GRANT ALL ON ojs.* TO 'ojs'@'localhost'" | mysql -uroot -pojs
 echo "FLUSH PRIVILEGES" | mysql -uroot -pojs
 
-# Download OJS and prepare it for installation
+# Prepare for OJS download and installation
 cd /var/www/html
-git clone https://github.com/pkp/ojs
+mkdir ojs
+chown ojs:ojs ojs
+
+# Run the remainder of the OJS installation as the ojs user
+cd /var/www/html
+sudo -u ojs git clone https://github.com/pkp/ojs ojs
 cd ojs
-./tools/startSubmodulesTRAVIS.sh
-cp config.TEMPLATE.inc.php config.inc.php
-chown -R www-data:www-data .
-chmod -R ug+w .
+sudo -u ojs ./tools/startSubmodulesTRAVIS.sh
+sudo -u ojs cp config.TEMPLATE.inc.php config.inc.php
+chmod -R ug+w cache public
 
 # Install Composer dependencies
 cd lib/pkp
@@ -21,10 +28,9 @@ curl -sS https://getcomposer.org/installer | php
 php composer.phar update
 
 # Create a handy symlink from the home directory
-cd ~vagrant
-ln -s /var/www/html/ojs .
+cd /home/ojs
+sudo -u ojs ln -s /var/www/html/ojs .
 
 # Create a files directory and set permissions on it
-mkdir files
-chown -R www-data:www-data files
+sudo -u ojs mkdir files
 chmod -R ug+w files
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,6 @@
 # Update Ubuntu
-apt-get -y update && apt-get -y upgrade
+apt-get -y update
+#apt-get -y upgrade
 
 # Install some useful stuff
 apt-get -y install openssh-server git vim wget curl
@@ -9,8 +10,23 @@ debconf-set-selections <<< 'mysql-server mysql-server/root_password password ojs
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password ojs'
 
 # Install LAMP server
-tasksel install lamp-server
-usermod -a -G www-data vagrant
+apt-get -y install php5-cgi libapache2-mod-fastcgi apache2-suexec-custom apache2-mpm-prefork
+
+# Configure Apache/FastCGI/suPHP
+a2enmod actions fastcgi suexec
+echo "Action application/x-httpd-php /cgi-bin/php.fcgi
+SuexecUserGroup ojs ojs" | tee /etc/apache2/sites-enabled/php-fcgi.conf
+mkdir cgi-bin
+echo "#!/bin/sh
+export PHP_FCGI_CHILDREN=4
+export PHP_FCGI_MAX_REQUESTS=200
+exec /usr/bin/php5-cgi" > cgi-bin/php.fcgi
+chmod -R 755 cgi-bin
+sed -i -e "s,#FastCgiWrapper /usr/lib/apache2/suexec,FastCgiWrapper /usr/lib/apache2/suexec,g" /etc/apache2/mods-enabled/fastcgi.conf
+sed -i -e "s,/var/www,$(pwd),g" /etc/apache2/sites-available/default
+sed -i -e "s,/usr/lib/cgi-bin,$(pwd)/cgi-bin,g" /etc/apache2/sites-available/default
+sed -i -e "s,\${APACHE_LOG_DIR},$(pwd),g" /etc/apache2/sites-available/default
+sed -i -e "s,/var/www,$(pwd)/,g" /etc/apache2/suexec/www-data
 
 # Install some additional PHP packages
 apt-get -y install php5-gd php5-dev php5-xsl php-soap php5-curl
