b0c095ba5086b99b2626abf5db5ae9ecbbd997ea
Alec Smecher <alec@smecher.bc.ca>
2019-01-22 11:40:41 -0800

    Update to PHP7.2; make app and branch configurable

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,13 +1,22 @@
 set -xe
 
+APPLICATION=$1
+BRANCH=$2
+
 export TEST=mysql
 
 echo "Installing packages..."
 sudo add-apt-repository ppa:jonathonf/firefox-esr-45
+
+sudo locale-gen en_US.UTF-8
+export LANG=en_US.UTF-8
+sudo add-apt-repository -y ppa:ondrej/php
+
 sudo apt-get -y update
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password temp_root_password'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password temp_root_password'
-sudo apt-get -y install openssh-server git vim wget curl tasksel php socat xvfb x11-utils default-jre composer zip php-zip php-curl php-mbstring mysql-server x11vnc php-dom firefox-esr php-mysql php-mysqli
+sudo apt-get -y install openssh-server git vim wget curl tasksel php socat xvfb x11-utils default-jre composer zip php-zip php-curl php-mbstring mysql-server x11vnc firefox-esr php-mysql php-xml
+sudo apt-get purge -y apache2
 mysqladmin -u root -p'temp_root_password' password ''
 
 echo "Installing NodeJS toolset..."
@@ -15,8 +24,8 @@ curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 echo "Installing source code..."
-git clone https://github.com/pkp/ojs ojs
-cd ojs
+git clone -b ${BRANCH} --single-branch https://github.com/pkp/${APPLICATION} ${APPLICATION}
+cd ${APPLICATION}
 ./tools/startSubmodulesTRAVIS.sh
 
 echo "Preparing the server environment..."
